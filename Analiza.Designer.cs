9365133ea8ac0ebe7359c417affa3283ef97cd92
Andrej <andrejkov007@gmail.com>
2025-05-29 20:48:06 +0200

    graph

--- Analiza.Designer.cs
+++ Analiza.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.izlazBtn = new System.Windows.Forms.Button();
             this.statistikaChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.statistikaChart)).BeginInit();
@@ -48,17 +48,17 @@
             // 
             // statistikaChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.statistikaChart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.statistikaChart.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.statistikaChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.statistikaChart.Legends.Add(legend2);
             this.statistikaChart.Location = new System.Drawing.Point(12, 12);
             this.statistikaChart.Name = "statistikaChart";
             this.statistikaChart.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.statistikaChart.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.statistikaChart.Series.Add(series2);
             this.statistikaChart.Size = new System.Drawing.Size(673, 426);
             this.statistikaChart.TabIndex = 1;
             this.statistikaChart.Text = "chart1";
@@ -72,6 +72,7 @@
             this.Controls.Add(this.izlazBtn);
             this.Name = "Analiza";
             this.Text = "Analiza";
+            this.Load += new System.EventHandler(this.Analiza_Load);
             ((System.ComponentModel.ISupportInitialize)(this.statistikaChart)).EndInit();
             this.ResumeLayout(false);
 
--- Analiza.cs
+++ Analiza.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace EvidenciaZamestnancov
 {
@@ -17,9 +19,69 @@ namespace EvidenciaZamestnancov
             InitializeComponent();
         }
 
+        private void Analiza_Load(object sender, EventArgs e)
+        {
+            using (var conn = Database.DbConnection())
+            {
+                conn.Open();
+
+                var query = "SELECT r.Ime AS ime, " +
+                                   "rs.DatumPostavljanja AS datumPostavljanja, " +
+                                   "rs.DatumRazresenja AS datumRazresenja " +
+                            "FROM radnik r " +
+                            "JOIN rukovodi_sektorom rs ON rs.RadnikID = r.RadnikID ";
+
+                using (var cmd =  new MySqlCommand(query, conn))
+                {
+                    var adapter = new MySqlDataAdapter(cmd);
+                    var dt = new DataTable();
+
+                    adapter.Fill(dt);
+
+                    statistikaChart.Series.Clear();
+                    statistikaChart.ChartAreas.Clear();
+                    statistikaChart.Legends.Clear();
+
+                    ChartArea oblast = new ChartArea();
+
+                    oblast.AxisY.Title = "Dni";
+                    oblast.AxisX.Title = "Rukovodilac";
+
+                    oblast.AxisX.MajorGrid.LineColor = Color.Black;
+                    oblast.AxisY.MajorGrid.LineColor = Color.Black;
+
+                    statistikaChart.ChartAreas.Add(oblast);
+
+                    Series seria = new Series()
+                    {
+                        ChartType = SeriesChartType.Column,
+                        XValueType = ChartValueType.String,
+                        YValueType = ChartValueType.Int64,
+                        BorderWidth = 2,
+                        Color = Color.Blue,
+                        IsValueShownAsLabel = false
+                    };
+
+                    foreach (DataRow riadok in dt.Rows)
+                    {
+                        string rukovodilac = riadok["ime"].ToString();
+                        DateTime datumRazresenja = Convert.ToDateTime(riadok["datumRazresenja"]);
+                        DateTime datumPostavljanja = Convert.ToDateTime(riadok["datumPostavljanja"]);
+
+                        int zostaloDni = (datumRazresenja - datumPostavljanja).Days;
+
+                        seria.Points.AddXY(rukovodilac, zostaloDni);
+                    }
+
+                    statistikaChart.Series.Add(seria);
+                }
+            }
+        }
+
         private void izlazBtn_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
     }
 }
